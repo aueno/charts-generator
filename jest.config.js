4fa682b44bd4168bcdbc7055be4527a177321294
aueno <bluesky-1415_ao@outlook.com>
2025-10-21 12:45:52 +0900

    fix: Jest settings

--- jest.config.js
+++ jest.config.js
@@ -2,23 +2,23 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        jsx: 'react-jsx'
+      }
+    }
+  },
+
   setupFilesAfterEnv: ['<rootDir>/jest.setup.tsx'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.json',
-        isolatedModules: true, // ← tsconfigに移しても良い
-        useESM: false,
-      },
-    ],
-  },
+
   transformIgnorePatterns: [
-    '/node_modules/(?!(@sgratzl|recharts|react-katex)/)', // ← 外部ESMライブラリを変換対象にする
+    '/node_modules/(?!(@sgratzl|recharts|react-katex)/)',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
